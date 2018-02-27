386d29d75406996b1f9da4783a8a39df8dc0f9ef
Greg Marvin <gregmarv@gmail.com>
2018-02-27 15:31:18 -0700

    yup

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
 
     <Provider store={store}>
+        <HashRouter>
         <App />
+        </HashRouter>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
--- stage-4/src/redux/reducer.js
+++ stage-4/src/redux/reducer.js
@@ -13,7 +13,7 @@ let initialState = {
 }
 
 // Reducer
-export default function reducer(state, action) {
+export default function reducer(state=initialState, action) {
     switch(action.type) {
         case GET_ALL_PRODUCTS_PENDING:
             return Object.assign({}, state, {loading: true});
@@ -27,7 +27,7 @@ export default function reducer(state, action) {
         case REMOVE_FROM_SHOPPING_CART:
             let newArray = state.shoppingCart.slice();
             newArray.splice(action.index, 1);
-            return Object.assign({}, {shoppingCart: newArray});
+            return Object.assign({}, state, {shoppingCart: newArray});
             
         default:
             return state;
